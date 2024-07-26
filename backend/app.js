b25552fc80365868911bf2155a77f6c1ddc9a47d
Srijan <97403569+Srijan-crypto@users.noreply.github.com>
2024-07-26 21:11:02 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({path:'*'}));
+app.use(cors({origin:'*'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
 
